2db9d066a6eadc534cf831ccb05273616ab15c37
mikoto2000 <mikoto2000@gmail.com>
2018-05-04 02:41:11 +0900

    make.bat に clean コマンドを追加。

--- make.bat
+++ make.bat
@@ -1,2 +1,11 @@
 @echo off
-docker-compose run --rm sphinx sphinx-build -b html -d build/doctrees source build/html -D html_enable_livereload=1
+
+if "%1" == "clean" (
+    for /d %%i in (work\build\*) do rmdir /q /s %%i
+    del /q /s work\build\*
+    goto end
+)
+
+docker-compose run --rm sphinx sphinx-build -N -b html -d build/doctrees source build/html -D html_enable_livereload=1
+
+:end
