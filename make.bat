b406c425be336cf33ac6491898cf51bb744e007d
mikoto2000 <mikoto2000@gmail.com>
2018-05-25 08:04:53 +0900

    Updated `make.bat`.

--- make.bat
+++ make.bat
@@ -6,6 +6,16 @@ if "%1" == "clean" (
     goto end
 )
 
-docker-compose run --rm sphinx sphinx-build -N -b html -d build/doctrees source build/html -D html_enable_livereload=1
+if "%1" == "open" (
+    start work\build\html\index.html
+    goto end
+)
+
+if "%1" == "livereload" (
+    docker-compose run --rm sphinx sphinx-build -N -b html -d build/doctrees source build/html -D html_enable_livereload=1
+    goto end
+)
+
+docker-compose run --rm sphinx make html
 
 :end
